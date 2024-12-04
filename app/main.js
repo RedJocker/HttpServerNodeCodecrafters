a4f661debe13fce909b4a991a1bfade46c548ee7
redjocker <mauricio.guaruja@gmail.com>
2024-12-04 02:37:56 -0300

    Refactor: uncommenting initial template

--- app/main.js
+++ app/main.js
@@ -1,13 +1,21 @@
-const net = require("net");
+import { createServer } from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//   });
-// });
-//
-// server.listen(4221, "localhost");
+const server = createServer((socket) => {
+    socket.on("close", () => {
+	socket.end();
+    });
+
+    socket.on('error', (err) => {
+	console.log(`socket on error: ${err}`);
+    });
+
+ 
+});
+
+console.log("Logs from your program will appear here!");
+
+server.listen(4221, "localhost");
